a580ed26b4f3aa82e3a1813c0e43fa616db3ea37
Dylan C. Kendal <dylankendal@gmail.com>
2023-10-02 19:23:32 -0400

    mix new

---
+++ lib/tree_sitter.ex
@@ -0,0 +1,18 @@
+defmodule TreeSitter do
+  @moduledoc """
+  Documentation for `TreeSitter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TreeSitter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/tree_sitter/application.ex
@@ -0,0 +1,20 @@
+defmodule TreeSitter.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: TreeSitter.Worker.start_link(arg)
+      # {TreeSitter.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: TreeSitter.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
