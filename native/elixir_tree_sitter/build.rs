dc77efd9b01489ca36df35b808b61c44c0dda5e6
Dylan C. Kendal <dylankendal@gmail.com>
2023-10-04 21:58:03 -0400

    Track grammar changes

--- native/elixir_tree_sitter/build.rs
+++ native/elixir_tree_sitter/build.rs
@@ -3,8 +3,10 @@ fn main() {
     let package = format!("tree-sitter-{}", language);
     let source_directory = format!("{}/src", package);
     let source_file = format!("{}/parser.c", source_directory);
+    let grammar_file = format!("{}/grammar.js", package);
 
-    println!("cargo:rerun-if-changed={}", source_file); // <1>
+    println!("cargo:rerun-if-changed={}", source_file);
+    println!("cargo:rerun-if-changed={}", grammar_file);
 
     cc::Build::new()
         .file(source_file)
